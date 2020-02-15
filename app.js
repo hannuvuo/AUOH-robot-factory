5cfb0e7f44f5740b54e18309e0b9f3253c102be7
hannuvuo <hannu.vuorenmaa@seamk.fi>
2020-02-15 15:47:00 +0200

    2

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ const axios = require('axios');
 axios.get('https://fanuc-robot-http-server.herokuapp.com/') .then((res) => { console.log(res); });
 
 //Lue löydetyt nivelarvo tekstit numeroina
-const regexp = 'joint (0-9)';
+const regexp = 'joint 9';
 let joints = [];
 let matches = res.data.matchAll(regexp);
-let count = 0; for (const match of matches) { count++;
+let count = 0;
+for (const match of matches) { count++;
     if (count > 6)
     break;
 const value = parseFloat(match[1]);
-joints.push(value); }
+joints.push(value);
+};
 
 
 //Luo ohjelmaan luuppi, joka pyörii ikuisesti
 const main_loop = () => { 
     setTimeout(() => { 
     main_loop();
- }, 10); 
-}
+ }, 1000); 
+};
 main_loop();
 
 //Tulosta jokaisella kierroksella aikaleima, nivelarvot lista ja pyyntöön kulunut aika
