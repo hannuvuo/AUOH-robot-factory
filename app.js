b6dc56baef05179325ca481a134f7ce353171ea8
hannuvuo <hannu.vuorenmaa@seamk.fi>
2020-03-07 14:58:10 +0200

    push

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 const axios = require('axios');
-axios.get('https://fanuc-robot-http-server.herokuapp.com/') .then((res) => { console.log(res); });
+const mqtt = require('mqtt');
 
-//Lue löydetyt nivelarvo tekstit numeroina
-const regexp = 'joint 9';
-let joints = [];
-let matches = res.data.matchAll(regexp);
-let count = 0;
-for (const match of matches) { count++;
-    if (count > 6)
-    break;
-const value = parseFloat(match[1]);
-joints.push(value);
-};
+const mqtt_client = mqtt.connect('wss://auoh-mqtt-broker-hv.herokuapp.com');
+mqtt_client.on('connect', () => { console.log('connected to mqtt broker');
+main_loop(); });
 
+const main_loop = () => {
+    setTimeout(() => {
+        let start_time_stamp = new Date(); 
 
-//Luo ohjelmaan luuppi, joka pyörii ikuisesti
-const main_loop = () => { 
-    setTimeout(() => { 
-    main_loop();
- }, 1000); 
-};
+        axios.get('https://fanuc-robot-http-server.herokuapp.com/')
+            .then((res) => {
+                const time_stamp = new Date();
+                const delta = time_stamp - start_time_stamp;
+                start_time_stamp = time_stamp;
+                const regexp = 'Joint   [1-6]: *(-?.*)';
+                let joints = [];
+                let matches = res.data.matchAll(regexp);
+                let count = 0;
+                for (const match of matches) {
+                    count++;
+                    if (count > 6) break;
+                    const value = parseFloat(match[1]);
+                    joints.push(value);
+                }
+                let data = { time: time_stamp, joints: joints };
+                console.log(start_time_stamp, joints, delta, 'ms');
+                mqtt_client.publish('joints', JSON.stringify(data));
+                main_loop();
+            });
+    }, 10);
+}
+
 main_loop();
-
-//Tulosta jokaisella kierroksella aikaleima, nivelarvot lista ja pyyntöön kulunut aika
-
-//Alla vinkkejä aikaleiman luomiseen ja viiveen laskemiseen
-const time_stamp = new Date();
-const delta = time_stamp - start_time_stamp;
-
-//Lopputuloksen pitäisi näyttää alla olevan mukaiselta
